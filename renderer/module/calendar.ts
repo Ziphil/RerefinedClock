ac9bd655234086524085a3d1f06e9c38187d8b75
Ziphil <yfe23457@nifty.com>
2020-05-27 12:11:51 +0900

    グレゴリオ暦を扱うクラスを作成

--- renderer/module/calendar.ts
+++ renderer/module/calendar.ts
@@ -29,17 +29,34 @@ export class HairianCalendar extends Calendar {
   protected calculate(date: Date): void {
     let genesis = new Date(2012, 0, 23);
     let basis = new Date(new Date().setHours(0, 0, 0, 0));
-    let count = FloorMath.div(date.getTime() - genesis.getTime(), 24 * 60 * 60 * 1000) + 547863;
-    let tick = Math.floor((date.getTime() - basis.getTime()) / 1000 / 86400 * 100000);
-    let rawYear = FloorMath.div(count * 4 + 3 + FloorMath.div((FloorMath.div((count + 1) * 4, 146097) * 3 + 1) * 4, 4), 1461);
-    let remainder = count - (rawYear * 365 + FloorMath.div(rawYear, 4) - FloorMath.div(rawYear, 100) + FloorMath.div(rawYear, 400));
+    let dayCount = FloorMath.div(date.getTime() - genesis.getTime(), 24 * 60 * 60 * 1000) + 547863;
+    let secondCount = Math.floor((date.getTime() - basis.getTime()) / 1000 / 86400 * 100000);
+    let rawYear = FloorMath.div(dayCount * 4 + 3 + FloorMath.div((FloorMath.div((dayCount + 1) * 4, 146097) * 3 + 1) * 4, 4), 1461);
+    let remainder = dayCount - (rawYear * 365 + FloorMath.div(rawYear, 4) - FloorMath.div(rawYear, 100) + FloorMath.div(rawYear, 400));
     this.year = rawYear + 1;
     this.month = FloorMath.div(remainder, 33) + 1;
     this.day = FloorMath.mod(remainder, 33) + 1;
-    this.hairia = count - 547862;
-    this.hour = FloorMath.div(tick, 10000);
-    this.minute = FloorMath.div(FloorMath.mod(tick, 10000), 100);
-    this.second = FloorMath.mod(tick, 10000);
+    this.hairia = dayCount - 547862;
+    this.hour = FloorMath.div(secondCount, 10000);
+    this.minute = FloorMath.div(FloorMath.mod(secondCount, 10000), 100);
+    this.second = FloorMath.mod(secondCount, 10000);
+  }
+
+}
+
+
+export class GregorianCalendar extends Calendar {
+
+  protected calculate(date: Date): void {
+    let genesis = new Date(2012, 0, 23);
+    let dayCount = FloorMath.div(date.getTime() - genesis.getTime(), 24 * 60 * 60 * 1000);
+    this.year = date.getFullYear();
+    this.month = date.getMonth() + 1;
+    this.day = date.getDate();
+    this.hairia = dayCount + 1;
+    this.hour = date.getHours();
+    this.minute = date.getMinutes();
+    this.second = date.getSeconds();
   }
 
 }
