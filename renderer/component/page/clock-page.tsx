daa75438a57d77eb9b997ecc89c8fb2446c51852
Ziphil <yfe23457@nifty.com>
2020-05-27 12:12:36 +0900

    矢印キーで表示する暦を切り替えられるよう変更

--- renderer/component/page/clock-page.tsx
+++ renderer/component/page/clock-page.tsx
@@ -5,8 +5,13 @@ import {
   ReactNode
 } from "react";
 import {
+  GregorianCalendar,
   HairianCalendar
 } from "../../module/calendar";
+import {
+  LiteralType,
+  LiteralUtilType
+} from "../../module/literal-util";
 import {
   PageComponent
 } from "../component";
@@ -19,8 +24,24 @@ export class ClockPage extends PageComponent<Props, State> {
 
   private timer: any | null = null;
 
+  public state: State = {
+    mode: "gregorian"
+  };
+
   public async componentDidMount(): Promise<void> {
-    this.timer = setInterval(() => this.setState({}), 10);
+    this.timer = setInterval(() => {
+      this.setState({});
+    }, 10);
+    window.addEventListener("keydown", (event) => {
+      let key = event.key;
+      if (key === "ArrowRight") {
+        let mode = CalendarModeUtil.next(this.state.mode);
+        this.setState({mode});
+      } else if (key === "ArrowLeft") {
+        let mode = CalendarModeUtil.previous(this.state.mode);
+        this.setState({mode});
+      }
+    });
   }
 
   public async componentWillUnmount(): Promise<void> {
@@ -31,7 +52,16 @@ export class ClockPage extends PageComponent<Props, State> {
 
   public render(): ReactNode {
     let date = new Date();
-    let calendar = new HairianCalendar(date);
+    let calendar = (() => {
+      let mode = this.state.mode;
+      if (mode === "gregorian") {
+        return new GregorianCalendar(date);
+      } else if (mode === "hairian") {
+        return new HairianCalendar(date);
+      } else {
+        return new GregorianCalendar(date);
+      }
+    })();
     let node = (
       <div className="clock-page-root">
         <Clock calendar={calendar}/>
@@ -46,4 +76,9 @@ export class ClockPage extends PageComponent<Props, State> {
 type Props = {
 };
 type State = {
+  mode: CalendarMode
 };
+
+const CALENDAR_MODES = ["gregorian", "hairian", "stopwatch"] as const;
+let CalendarModeUtil = LiteralUtilType.create(CALENDAR_MODES);
+type CalendarMode = LiteralType<typeof CALENDAR_MODES>;
---
+++ renderer/module/literal-util.ts
@@ -0,0 +1,78 @@
+//
+
+import {
+  FloorMath
+} from "./floor-math";
+
+
+export class LiteralUtilType<T extends string> {
+
+  private values: Array<T>;
+  public defaultValue: T;
+  public is: (value: string) => value is T;
+
+  private constructor(values: Array<T>, defaultValue: T, is: (value: string) => value is T) {
+    this.values = values;
+    this.defaultValue = defaultValue;
+    this.is = is;
+  }
+
+  public cast(value: string | number): T {
+    if (typeof value === "string") {
+      if (this.is(value)) {
+        return value;
+      } else {
+        return this.defaultValue;
+      }
+    } else {
+      if (value >= 0 && value < this.values.length) {
+        return this.values[value];
+      } else {
+        return this.defaultValue;
+      }
+    }
+  }
+
+  public indexOf(value: string): number {
+    let anyValue = value as any;
+    let index = this.values.indexOf(anyValue);
+    return index;
+  }
+
+  public previous(value: string): T {
+    let anyValue = value as any;
+    let index = this.values.indexOf(anyValue);
+    if (index >= 0) {
+      let previousIndex = FloorMath.mod(index - 1, this.values.length);
+      return this.values[previousIndex];
+    } else {
+      return this.defaultValue;
+    }
+  }
+
+  public next(value: string): T {
+    let anyValue = value as any;
+    let index = this.values.indexOf(anyValue);
+    if (index >= 0) {
+      let nextIndex = FloorMath.mod(index + 1, this.values.length);
+      return this.values[nextIndex];
+    } else {
+      return this.defaultValue;
+    }
+  }
+
+  public static create<T extends string>(values: {0: T} & ArrayLike<T>): LiteralUtilType<T> {
+    let castValues = Array.from(values);
+    let defaultValue = values[0];
+    let is = function (value: string): value is T {
+      let anyValue = value as any;
+      return castValues.indexOf(anyValue) >= 0;
+    };
+    let result = new LiteralUtilType(castValues, defaultValue, is);
+    return result;
+  }
+
+}
+
+
+export type LiteralType<T> = T extends {[key: number]: infer U} ? U : never;
